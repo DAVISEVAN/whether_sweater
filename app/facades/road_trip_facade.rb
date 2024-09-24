d61380ac738e7193133d9c5f05d8ee01c9fc4d74
Evan Davis <evandavis427@gmail.com>
2024-09-24 17:03:36 -0600

    tests for road_trip end point

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -2,15 +2,17 @@ class RoadTripFacade
   def self.create_road_trip(origin, destination)
     route_data = DirectionsService.get_route(origin, destination)
 
-    if route_data[:info][:statuscode] == 0
+    # Check if route data is missing, incomplete, or indicates an error
+    if route_data[:route].nil? || route_data[:route][:formattedTime].nil? || route_data[:info][:statuscode] != 0
+      Rails.logger.info("Detected an impossible route from #{origin} to #{destination}.")
+      RoadTrip.new(origin, destination, "impossible route", {})
+    else
       travel_time = route_data[:route][:formattedTime]
       end_location = route_data[:route][:locations].last[:latLng]
       weather_data = WeatherService.get_forecast(end_location[:lat], end_location[:lng])
 
       eta = calculate_eta(weather_data, travel_time)
       RoadTrip.new(origin, destination, travel_time, eta)
-    else
-      RoadTrip.new(origin, destination, "impossible route", {})
     end
   end
 
@@ -20,18 +22,8 @@ class RoadTripFacade
 
     eta_weather = weather_data[:forecast][:forecastday].flat_map { |day| day[:hour] }[total_hours]
 
-    
-    if eta_weather.nil?
-      {}
-    else
-      {
-        datetime: eta_weather[:time],
-        temperature: eta_weather[:temp_f],
-        condition: eta_weather[:condition][:text]
-      }
-    end
+    eta_weather.nil? ? {} : { datetime: eta_weather[:time], temperature: eta_weather[:temp_f], condition: eta_weather[:condition][:text] }
   rescue
-    
     {}
   end
 end
--- app/services/directions_service.rb
+++ app/services/directions_service.rb
@@ -6,9 +6,29 @@ class DirectionsService
       req.params['to'] = destination
     end
 
-    JSON.parse(response.body, symbolize_names: true)
-  rescue Faraday::Error, JSON::ParserError => e
+    data = JSON.parse(response.body, symbolize_names: true) rescue {}
+
+    
+    Rails.logger.info("Mocked Directions API Response: #{data}")
+
+   
+    status_code = data.dig(:info, :statuscode)
+    if status_code.nil?
+      Rails.logger.error("Invalid API response format or missing status code: #{data}")
+      return { info: { statuscode: 500 }, route: nil }
+    end
+
+    Rails.logger.info("Directions API Response Status: #{status_code}")
+
+    if status_code == 0 && data[:route].present?
+      Rails.logger.info("Handling as a possible route with travel time: #{data[:route][:formattedTime]}")
+      data
+    else
+      Rails.logger.info("Handling as an impossible route due to missing or incomplete data.")
+      { info: { statuscode: status_code }, route: nil }
+    end
+  rescue Faraday::Error => e
     Rails.logger.error("DirectionsService Error: #{e.message}")
-    { info: { statuscode: 500 } }
+    { info: { statuscode: 500 }, route: nil }
   end
 end
